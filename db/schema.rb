ddd6026a98785e9cc43c1972607ea6f09b62d17f
Liuming <lfan.canada@gmail.com>
2022-01-17 21:09:25 +0000

    add category_id to plays table, db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_204842) do
+ActiveRecord::Schema.define(version: 2022_01_17_210818) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_01_17_204842) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "category_id"
   end
 
   create_table "users", force: :cascade do |t|
