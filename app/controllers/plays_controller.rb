372eb3c8b8e2ae3e89ce8de802de92a29ebb8acc
Liuming <lfan.canada@gmail.com>
2022-01-17 20:36:04 +0000

    change version of Bootstrap; update actions on PlaysController

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -6,14 +6,14 @@ class PlaysController < ApplicationController
   end
 
   def new
-    @play = current_user.play.build
+    @play = current_user.plays.build
   end
 
   def show
   end
 
   def create
-    @play = current_user.play.build(play_params)
+    @play = current_user.plays.build(play_params)
     if @play.save 
       redirect_to root_path    #redirect -> page refresh
     else
