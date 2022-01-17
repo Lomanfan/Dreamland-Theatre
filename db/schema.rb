765f3e1e586703434af436b5165fafda4a79356e
Liuming <lfan.canada@gmail.com>
2022-01-17 21:06:34 +0000

    add Category model, db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_17_193112) do
+ActiveRecord::Schema.define(version: 2022_01_17_204842) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
