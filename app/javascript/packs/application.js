4f94b907e18d0655991aa2b03befb26b5ef10b33
Liuming <lfan.canada@gmail.com>
2022-01-28 05:56:31 +0000

    add Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,20 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
 
-//= require jquery
-//= require bootstrap-sprockets
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import "bootstrap";
+import "../stylesheets/application.scss";
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+})
+
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
