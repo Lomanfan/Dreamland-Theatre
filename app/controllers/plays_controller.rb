b9f4007cb00e2d4bc611c6807715453289d73ccc
Liuming <lfan.canada@gmail.com>
2022-01-29 04:16:07 +0000

    update PlaysController index method

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,7 +2,12 @@ class PlaysController < ApplicationController
   before_action :find_play, only: [:show, :edit, :update, :destroy]
 
   def index
+    if params[:category].blank?
     @plays = Play.all.order("created_at DESC")
+    else 
+      @category_id = Category.find(name: params[:category]).id
+      @plays = Play.where(:category_id => @category_id).order("created_at DESC")
+    end 
   end
 
   def new
